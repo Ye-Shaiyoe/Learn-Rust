5cb1a8ed46d4862a902e3a77ab5671f1a2bfa376
Akram <akrommuhammadyusuf@gmail.com>
2026-03-11 23:44:11 +0700

    Update and rename nestedWwhile.rs to while.rs

---
+++ first_weak /loopping /while.rs
@@ -0,0 +1,19 @@
+// let mut i = 0;
+// let max = 5;
+
+// while i < max {
+//     let mut j = 0;
+//     let max_inner = i;
+
+//     while j <= max_inner {
+//         print!("* ");
+//         j += 1;
+//     }
+    
+//     println!();
+//     i += 1;
+// }
+
+use std::thread::sleep;
+use std::time::Duration;
+
