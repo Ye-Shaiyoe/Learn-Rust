90287456c11de3998ddc24587aa93ebad6bf8a0c
Akram <akrommuhammadyusuf@gmail.com>
2026-03-11 23:43:28 +0700

    Create nestedWwhile.rs

---
+++ first_weak /loopping /nestedWwhile.rs
@@ -0,0 +1,15 @@
+let mut i = 0;
+let max = 5;
+
+while i < max {
+    let mut j = 0;
+    let max_inner = i;
+
+    while j <= max_inner {
+        print!("* ");
+        j += 1;
+    }
+    
+    println!();
+    i += 1;
+}
